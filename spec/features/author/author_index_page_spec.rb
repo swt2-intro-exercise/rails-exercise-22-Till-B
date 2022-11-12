595d3a99fde645a63f036582bb7a8623c94243c4
Till-B <bergmann@uni-potsdam.de>
2022-11-12 15:04:21 +0100

    fifth issue third try

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe "Show index page", type: :feature do
   it "show index page" do
-    visit author_index_path
+    visit authors_path
   end
 
   it "Show overview of all authros" do
 
     @alan = FactoryBot.create :author
     @alan.save
-    visit author_index_path
+    visit authors_path
 
     expect(page).to have_text("Alan")
     expect(page).to have_text("Turing")
