3cd15ee81e03432b205a26fd29bfa0f6a9a94231
Till-B <bergmann@uni-potsdam.de>
2022-11-12 18:11:05 +0100

    15th issue

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,4 +5,6 @@ class Author < ApplicationRecord
     return first_name + " " + last_name
   end
 
+  has_and_belongs_to_many :papers
+
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Paper, type: :model do
     expect(paper.year).to eq(2022)
   end
 
+  it "should have an empty list of authors" do
+    paper = Paper.new(title: "Test", venue: "Potsdam", year: 2022)
+    expect(paper.authors).to be_empty
+  end
+
 end
